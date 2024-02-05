a540effe24eeeb9989d82364961fab3005e6c486
atisaff <safaeiatieh@yahoo.com>
2024-02-05 14:25:22 +0330

    Added forecast css

--- src/app.js
+++ src/app.js
@@ -51,26 +51,39 @@ function search(event) {
 
   axios.get(apiUrl).then(displayTemp);
 }
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Monday", "Tuseday", "Wednesday", "Thursday", "Friday"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      ` 
+   <div id="forecast-by-day"   
+    <div class="weather-forecast-date">${day}</div>
+      
+
+      <img 
+        src="https://cdn4.iconfinder.com/data/icons/the-weather-is-nice-today/64/weather_49-1024.png"
+        width="40"
+        class="weather-forecast-icon"
+      />
+     
+     
+      <div class="weather-forecast-temp">
+        <div class="weather-forecast-temp-max"> 18°</div>
+        <div class="weather-forecast-temp-min"> 12°</div>
+      </div>
+     </div> 
+      
+  
+`;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
-
-function getForecast(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
-}
-
-function displayForecast(response) {
-  console.log(response.data);
-}
-
-`<div class="weather-forecast-day">
-   <div class="weather-forecast-date">Tuseday</div>
-   <div class="weather-forecast-icon"></div>
-   <div class="weather-forecast-temperatures"></div>
-    <div class="weather-forecast-temperature"></div>
-     <strong>12</strong>
-    </div>
-    <div class="weather-forecast-temperature">9</div></div >
-  </div >
-</div >`;
+displayForecast();
